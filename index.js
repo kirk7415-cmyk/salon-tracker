f7616291b4d3f8651f085f39452b014c99a256bb
kirk7415-cmyk <kirk7415@gmail.com>
2026-05-19 02:26:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Noto Sans TC', 'Hiragino Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+
+button {
+  font-family: inherit;
+}
+
+input, select, textarea {
+  font-family: inherit;
+}
